80bf1f14ce84e87dca8a64fe2539743cf64c9900
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-08-21 16:53:59 +0300

    Add files via upload

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <main>
+      <p>
+        This application contains snippets from different articles related to Web Development and Computer Science. You can browse all the articles and read them in their entirety <a href="https://www.codecademy.com/articles">here</a>.
+      </p>
+    </main>
+  );
+}
